56952068d68537999d8ea9858f81af8f8f6a7f2f
Mars8.6 <meyer.alexandre83@gmail.com>
2017-03-20 17:44:05 +0100

    Changement du gitignore et ajout de la method post pour l'authentification

---
+++ android/app/src/main/java/com/example/jb/icarnfc/User.java
@@ -0,0 +1,19 @@
+package com.example.jb.icarnfc;
+
+/**
+ * Created by Mars on 20/03/2017.
+ */
+
+
+    class User {
+
+        final String UserLogin;
+        final String UserPassword;
+
+        public User(String UserLogin, String UserPassword) {
+            this.UserLogin = UserLogin;
+            this.UserPassword = UserPassword;
+
+        }
+    }
+
